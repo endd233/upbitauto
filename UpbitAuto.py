63d6ee6bedee9a4f42c975c1862ddfe47656538e
endd233 <97231403+endd233@users.noreply.github.com>
2022-01-14 16:14:32 +0900

    Add files via upload

--- UpbitAuto.py
+++ UpbitAuto.py
@@ -6,26 +6,24 @@ import os
 import numpy as np
 # os.chdir(r'C:\Users\brm22\Desktop\Python_world\Upbit')
 k = 0.5
-
 def max_k():
+    ror_1 = 0
     MAK_K = 0.1
     for k in range(1, 10):
-        ror_1 = 0
-        df = pyupbit.get_ohlcv("KRW-BTC", count=10)
-            # print(df)
+        df = pyupbit.get_ohlcv("KRW-BTC", count=5)
+        # print(df)
         df['range'] = (df['high'] - df['low']) * k/10
-            # print(df['range'])
+        # print(df['range'])
         df['target'] = df['open'] + df['range'].shift(1)
-            # print(df['target'])
+        # print(df['target'])
         df['ror'] = np.where(df['high'] > df['target'], df['close'] / df['target'], 1)
-            # print(df['ror'])
+        # print(df['ror'])
         ror = df['ror'].cumprod()[-2]
-            # print(ror)
+        # print(ror)
         ror = float(ror)
         if ror > ror_1:
             MAK_K = k/10
-
-        ror_1 = ror
+            ror_1 = ror
 
     return MAK_K
 
@@ -36,6 +34,8 @@ def cal_target(ticker):
     yesterday_range = yesterday['high'] - yesterday['low']
     target = today['open'] + yesterday_range * k
     return target
+
+
 
 # 객체 생성
 # f = open("upbit.txt", 'r')
@@ -55,12 +55,12 @@ while True:
     now = datetime.datetime.now()
 
     # 목표가 갱신 k값 설정
-    if now.hour == 9 and now.minute == 0 and 10 < now.second < 20: 
+    if now.hour == 9 and now.minute == 0 and 20 < now.second < 30: 
         k = max_k()
         target = cal_target("KRW-BTC")
         time.sleep(10)
         op_mode = True
-        print(f"비트코인 목표가를 {target}으로 k값을 {0}로 갱신했습니다.")
+        print(f"REFRESH! => target price :{target}, k :{k}")
 
     
     # 매초마다 조건확인후 매수
@@ -68,7 +68,7 @@ while True:
         krw_balance = upbit.get_balance("KRW")
         upbit.buy_market_order("KRW-BTC", krw_balance)
         hold = True
-        print(f"비트코인을 {now}에 {krw_balance}원 만큼 매수했습니다.")
+        print(f"PURCHASING! => purchase price: {krw_balance}, purchase time: {now}")
 
     # 매도
     if now.hour == 8 and now.minute == 59 and 0 < now.second < 10:
@@ -76,11 +76,11 @@ while True:
             btc_balance = upbit.get_balance("KRW-BTC")
             upbit.sell_market_order("KRW-BTC", btc_balance)
             hold = False
-            print(f"비트코인 {btc_balance} 만큼 매도했습니다.")
+            print(f"SELLING! => sell price: {btc_balance}")
         op_mode = False
         time.sleep(10)
 
-    print(f"현재시간: {now} 목표가: {target} 현재가: {price} 보유상태: {hold} 동작상태: {op_mode} k값 {k}")
+    # print(f"현재시간: {now} 목표가: {target} 현재가: {price} 보유상태: {hold} 동작상태: {op_mode} k값 {k}")
     
 
     time.sleep(1)
